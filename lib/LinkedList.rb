1a3831c011ea64a27f27b38c1ba57dfa4ee87d70
ali <ali.akeelqq123@gmail.com>
2024-10-07 10:43:31 +0300

    add prepend, size, head, tail, at, pop methods

--- lib/LinkedList.rb
+++ lib/LinkedList.rb
@@ -1,4 +1,4 @@
-require 'lib/Node.rb'
+require_relative 'Node.rb'
 
 class LinkedList
   def initialize
@@ -8,7 +8,7 @@ class LinkedList
 
   def append(value)
     new_node = Node.new(value)
-    if @head == nil
+    if @head.nil?
       @head = new_node
       @tail = new_node
     else
@@ -16,7 +16,64 @@ class LinkedList
       @tail = new_node
     end
   end
+
+  def prepend(value)
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+      @tail = new_node
+    else
+      new_node.next_node = @head
+      @head = new_node
+    end
+  end
+
+  def size
+    puts Node.size
+  end
+
+  def head
+    puts @head.value
+  end
+
+  def tail
+    puts @tail.value
+  end
+
+  def at(index)
+    current_node = @head
+    current_index = 0
+    while current_node != nil
+      return current_node.value if current_index == index
+      current_node = current_node.next_node
+      current_index += 1
+    end
+    nil
+  end
+
+  def pop
+    if head.nil?
+      return nil
+    elsif Node.size = 1
+      popped_node = @tail
+      @head = nil
+      @tail = nil
+      return popped_node
+    else
+      current_node = @head
+      while current_node.next_node != @tail
+        current_node = current_node.next_node
+      end
+    end
+    popped_node = @tail
+    @tail = current_node
+    @tail.next_node = nil
+    popped_node
+  end
 end
 
 list = LinkedList.new
-list.append(2)
+list.append(1)
+list.append(5)
+list.append(3)
+list.pop
--- lib/Node.rb
+++ lib/Node.rb
@@ -1,7 +1,13 @@
 class Node
+  @@node_count = 0
   attr_accessor :value, :next_node
   def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
+    @@node_count += 1
+  end
+
+  def self.size
+    @@node_count
   end
 end
